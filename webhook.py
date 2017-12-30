a0b07cbbfd12ff8145c041a10d7d19af0044ad2f
satsheroku <34965676+satsheroku@users.noreply.github.com>
2017-12-30 11:10:29 -0800

    Add files via upload

--- webhook.py
+++ webhook.py
@@ -32,7 +32,7 @@ def makeResponse(req):
     date = parameters.get("date")
     if city is None:
         return None
-    r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=b6907d289e10d714a6e88b30761fae22')
+    r=requests.get('http://samples.openweathermap.org/data/2.5/forecast?q='+city+'&appid=b6907d289e10d714a6e88b30761fae22')
     ####r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=06f070197b1f60e55231f8c46658d077')
     json_object = r.json()
     weather=json_object['list']
